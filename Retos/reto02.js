39358a3d0c791c5322817c7cd18b9b94ce45cc5a
damiancarrillo01 <damian.15carrillod@gmail.com>
2023-09-15 12:55:39 -0300

    act

--- Retos/reto02.js
+++ Retos/reto02.js
@@ -2,12 +2,23 @@ let nota1, nota2, nota3;
 
 do {
     nota1 = parseFloat(prompt("Ingrese la Nota 1:"));
+    if (isNaN(nota1) || nota1 < 1 || nota1 > 7) {
+        alert("La Nota 1 no es válida. Por favor, ingrese una nota entre 1 y 7.");
+    }
 } while (isNaN(nota1) || nota1 < 1 || nota1 > 7);
+
 do {
     nota2 = parseFloat(prompt("Ingrese la Nota 2:"));
+    if (isNaN(nota2) || nota2 < 1 || nota2 > 7) {
+        alert("La Nota 2 no es válida. Por favor, ingrese una nota entre 1 y 7.");
+    }
 } while (isNaN(nota2) || nota2 < 1 || nota2 > 7);
+
 do {
     nota3 = parseFloat(prompt("Ingrese la Nota 3:"));
+    if (isNaN(nota3) || nota3 < 1 || nota3 > 7) {
+        alert("La Nota 3 no es válida. Por favor, ingrese una nota entre 1 y 7.");
+    }
 } while (isNaN(nota3) || nota3 < 1 || nota3 > 7);
 
 const promedioPonderado = (nota1 * 0.4 + nota2 * 0.3 + nota3 * 0.3).toFixed(1);
