abf9fa707d81e91fea1f64e7e25af9df4e4c5f08
damiancarrillo01 <damian.15carrillod@gmail.com>
2023-09-15 23:53:20 -0300

    actualizacion reto2

--- Retos/reto02.js
+++ Retos/reto02.js
@@ -1,33 +1,38 @@
-let nota1, nota2, nota3;
 
-do {
-    nota1 = parseFloat(prompt("Ingrese la Nota 1:"));
-    if (isNaN(nota1) || nota1 < 1 || nota1 > 7) {
-        alert("La Nota 1 no es válida. Por favor, ingrese una nota entre 1 y 7.");
-    }
-} while (isNaN(nota1) || nota1 < 1 || nota1 > 7);
-
-do {
-    nota2 = parseFloat(prompt("Ingrese la Nota 2:"));
-    if (isNaN(nota2) || nota2 < 1 || nota2 > 7) {
-        alert("La Nota 2 no es válida. Por favor, ingrese una nota entre 1 y 7.");
-    }
-} while (isNaN(nota2) || nota2 < 1 || nota2 > 7);
-
-do {
-    nota3 = parseFloat(prompt("Ingrese la Nota 3:"));
-    if (isNaN(nota3) || nota3 < 1 || nota3 > 7) {
-        alert("La Nota 3 no es válida. Por favor, ingrese una nota entre 1 y 7.");
-    }
-} while (isNaN(nota3) || nota3 < 1 || nota3 > 7);
-
-const promedioPonderado = (nota1 * 0.4 + nota2 * 0.3 + nota3 * 0.3).toFixed(1);
-
-                
-if (promedioPonderado < 3.95) {
-    alert(`El estudiante ha reprobado la asignatura. Promedio: ${promedioPonderado}`);
-} else if (promedioPonderado >= 3.95 && promedioPonderado <= 4.94) {
-    alert(`El estudiante va a examen. Promedio: ${promedioPonderado}`);
-} else {
-    alert(`El estudiante se exime de la asignatura. Promedio: ${promedioPonderado}`);
+function validarNota(mensaje) {
+    let nota;
+    do {
+        nota = parseFloat(prompt(mensaje));
+        if (isNaN(nota) || nota < 1 || nota > 7) {
+            alert("La nota no es válida.ingrese una nota entre 1 y 7.");
+        }
+    } while (isNaN(nota) || nota < 1 || nota > 7);
+    return nota;
 }
+
+
+function calcularPromedioPonderado(nota1, nota2, nota3) {
+    return ((nota1 * 0.4) + (nota2 * 0.3) + (nota3 * 0.3)).toFixed(1);
+}
+
+
+function determinarResultado(promedio) {
+    if (promedio < 3.95) {
+        return "El estudiante ha reprobado la asignatura.";
+    } else if (promedio >= 3.95 && promedio <= 4.94) {
+        return "El estudiante debe rendir examen.";
+    } else {
+        return "El estudiante se exime de la asignatura.";
+    }
+}
+
+
+const nota1 = validarNota("Ingrese la Nota 1:");
+const nota2 = validarNota("Ingrese la Nota 2:");
+const nota3 = validarNota("Ingrese la Nota 3:");
+
+const promedioPonderado = calcularPromedioPonderado(nota1, nota2, nota3);
+
+const resultado = determinarResultado(promedioPonderado);
+
+alert(`${resultado} Promedio: ${promedioPonderado}`);
